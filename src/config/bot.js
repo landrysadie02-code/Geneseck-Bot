b6ea7fec82fa0336da5589b350feedf63ae85755
landrysadie02-code <landrysadie02@gmail.com>
2026-06-10 00:30:26 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "I Wish I Was The Real Geneseck...",
         // Activity type number (0 = Playing).
         type: 0, 
       },
