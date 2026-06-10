59b6ccf694149e9651731d2e51721e59a8d2f52f
landrysadie02-code <landrysadie02@gmail.com>
2026-06-10 01:32:00 -0400

    Update Blackjack.js

--- src/commands/Economy/Blackjack.js
+++ src/commands/Economy/Blackjack.js
@@ -1,1 +1,23 @@
+// /blackjack command
 
+// Create deck
+// Shuffle deck
+// Deal cards
+
+// Player: 2 cards
+// Dealer: 2 cards
+
+// Send embed showing:
+// Dealer: [K] [?]
+// Player: [10] [7]
+
+// Add Hit and Stand buttons
+
+// If Hit:
+// Draw card
+// Update embed
+
+// If Stand:
+// Dealer draws until 17
+// Compare totals
+// Announce winner
